ea90f1e87c53829fe1f7ee181566579bce044912
satomikenta-org <45529968+satomikenta-org@users.noreply.github.com>
2025-02-19 10:14:34 +0000

    set up react

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  console.log("レンダリング!!!")
+
+  return (
+    <>
+      <h1>React.js</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
